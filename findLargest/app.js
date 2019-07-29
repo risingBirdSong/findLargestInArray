6762daafe2f435329b3b35206f225280fe102d16
Peter Murphy <naughtnowwhen@gmail.com>
2019-07-29 13:25:30 -0700

    about to try to add async await

--- findLargest/app.js
+++ findLargest/app.js
@@ -3,10 +3,15 @@ function findLargest(numbers) {
     var max = numbers[0];
     for (var _i = 0, numbers_1 = numbers; _i < numbers_1.length; _i++) {
         var num = numbers_1[_i];
+        var tagIt = "<p>" + num + "</p>";
+        var tagItH1 = "<h1>" + num + "</h1>";
+        $(".current_target").html(num.toString());
+        $(".inside_array").append(tagIt);
+        $(".inside_array").append("<p>,</p>");
         if (num > max) {
             max = num;
         }
     }
     return max;
 }
-console.log(findLargest([1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]));
+var tryit = findLargest([5, 3, 3, 6, 5, 9, 15]);
--- findLargest/app.ts
+++ findLargest/app.ts
@@ -1,15 +1,13 @@
 
 
-
-
-
-
-
-
-
 function findLargest(numbers: number[]) {
   let max = numbers[0];
   for (let num of numbers) {
+    let tagIt = `<p>${num}</p>`;
+    let tagItH1 = `<h1>${num}</h1>`;
+    $(".current_target").html(num.toString());
+    $(".inside_array").append(tagIt);
+    $(".inside_array").append("<p>,</p>");
     if (num > max) {
       max = num;
     }
@@ -17,4 +15,4 @@ function findLargest(numbers: number[]) {
   return max;
 }
 
-console.log(findLargest([1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]));
+let tryit = findLargest([5, 3, 3, 6, 5, 9, 15]);
